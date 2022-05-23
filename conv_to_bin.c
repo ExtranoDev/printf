ecbe9dbcd38295d2500b44e0c5a09074077c3660
ExtranoDev <jeomade19@gmail.com>
2022-05-23 03:02:31 +0000

    To Binary converter

---
+++ conv_to_bin.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * conv_to_bin - prints binary representation of a number
+ * @num: number
+ * @nump: number of char to be printed
+ */
+void conv_to_bin(unsigned int num, unsigned int *nump)
+{
+	if (num > 1)
+	{
+		*nump += 1;
+		conv_to_bin(num >> 1, nump);
+	}
+	_putchar((num & 1) + '0');
+}

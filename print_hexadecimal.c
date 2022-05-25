5d2395a4f0a08b6a6d04809269befa435b95637e
Oguntola Joshua <101347375+ExtranoDev@users.noreply.github.com>
2022-05-25 13:05:23 -0700

    Updated

--- print_hexadecimal.c
+++ print_hexadecimal.c
@@ -1,111 +1,66 @@
 #include "main.h"
 
 /**
- * print_unsignedIntToHex - prints unsigned int to hexadecimal.
- * @num: number to print
- * @_case: letter `a` on the case to print it (upper or lower)
- * Return: number or char printed
+ * print_hex - prints a number in hexadecimal base,
+ * in lowercase
+ * @l: va_list arguments from _printf
+ * @f: pointer to the struct flags that determines
+ * if a flag is passed to _printf
+ * Description: the function calls convert() which in turns converts the input
+ * number into the correct base and returns it as a string
+ * Return: the number of char printed
  */
-int print_unsignedIntToHex(unsigned int num, char _case)
+int print_hex(va_list l, flags_t *f)
 {
-	unsigned int num2;
-	int i, j, remainder, nbrCharacters = 0;
-	char *numhex;
+    unsigned int num = va_arg(l, unsigned int);
+    char *str = convert(num, 16, 1);
+    int count = 0;
 
-	for (num2 = num; num2 != 0; nbrCharacters++, num2 /= 16)
-	;
-
-	numhex = malloc(nbrCharacters);
-	for (i = 0; num != 0; i++)
-	{
-		remainder = num % 16;
-		if (remainder < 10)
-			numhex[i] = remainder + '0';
-		else
-			numhex[i] = remainder - 10 + _case;
-		num = num / 16;
-	}
-	for (j = i - 1; j >= 0; j--)
-		_putchar(numhex[j]);
-	free(numhex);
-	return (nbrCharacters);
-}
-/**
- * print_hex_base - base function for printing hexadecimal numbers
- * @arg: argument list containing hexadecimal
- * @_case: a  in caps on lower, printing the hex number in caps in lower
- * Return: number of digits printed
- */
-
-int print_hex_base(va_list arg, char _case)
-{
-	unsigned int num = va_arg(arg, unsigned int);
-	int nbrCharacters;
-
-	if (num == 0)
-		return (_putchar('0'));
-	nbrCharacters = print_unsignedIntToHex(num, _case);
-
-	return (nbrCharacters);
+    if (f->hash == 1 && str[0] != '0')
+        count += _puts("0x");
+    count += _puts(str);
+    return (count);
 }
 
 /**
- * print_hex - prints a hexadecimal in lower case
- * @arg: list that contains hexadecimal to print
- * Return: number of digits printed
+ * print_hex_big - prints a number in hexadecimal base,
+ * in uppercase
+ * @l: va_list arguments from _printf
+ * @f: pointer to the struct that determines
+ * if a flag is passed to _printf
+ * Description: the function calls convert() which in turns converts the input
+ * number into the correct base and returns it as a string
+ * Return: the number of char printed
  */
-int print_hex(va_list arg)
+int print_hex_big(va_list l, flags_t *f)
 {
-	return (print_hex_base(arg, 'a'));
+    unsigned int num = va_arg(l, unsigned int);
+    char *str = convert(num, 16, 0);
+    int count = 0;
+
+    if (f->hash == 1 && str[0] != '0')
+        count += _puts("0X");
+    count += _puts(str);
+    return (count);
 }
 
 /**
- * print_HEX - prints a hexadecimal in upper case
- * @arg: list that contains hexadecimal to print
- * Return: number of digits printed
+ * print_octal - prints a number in base 8
+ * @l: va_list arguments from _printf
+ * @f: pointer to the struct that determines
+ * if a flag is passed to _printf
+ * Description: the function calls convert() which in turns converts the input
+ * number into the correct base and returns it as a string
+ * Return: the number of char printed
  */
-
-int print_HEX(va_list arg)
+int print_octal(va_list l, flags_t *f)
 {
-	return (print_hex_base(arg, 'A'));
-}
+    unsigned int num = va_arg(l, unsigned int);
+    char *str = convert(num, 8, 0);
+    int count = 0;
 
-/**
- * print_oct - prints number in octal base.
- * @arg: list of num in octal base
- * Return: number of octals printed
- */
-
-int print_oct(va_list arg)
-{
-	unsigned int num = va_arg(arg, unsigned int);
-	unsigned int dup;
-	char *octa;
-	int i, j, charPrinted = 0;
-
-	if (num == 0)
-		return (_putchar('0'));
-	for (dup = num; dup != 0; j++)
-	{
-		dup /= 8;
-	}
-	octa = malloc(j);
-	if (!octa)
-		return (-1);
-
-	for (i = j - 1; i >= 0; i--)
-	{
-		octa[i] = num % 8 + '0';
-		num = num / 8;
-	}
-
-	for (i = 0; i < j && octa[i] == '0'; i++)
-		;
-	for (; i < j; i++)
-	{
-		_putchar(octa[i]);
-		charPrinted++;
-	}
-	free(octa);
-	return (charPrinted);
+    if (f->hash == 1 && str[0] != '0')
+        count += _putchar('0');
+    count += _puts(str);
+    return (count);
 }

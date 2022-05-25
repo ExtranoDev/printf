4d12afc3c1be1245dc4925cb00ed8d341af2d06f
Oguntola Joshua <101347375+ExtranoDev@users.noreply.github.com>
2022-05-25 14:19:22 -0700

    Update printers

--- numbers_printer.c
+++ numbers_printer.c
@@ -1,84 +1,132 @@
 #include "main.h"
 
 /**
- * print_int - prints an integer
- * @l: va_list of arguments from _printf
- * @f: pointer to the struct flags determining
- * if a flag is passed to _printf
- * Return: number of char printed
+ * print_char - prints character
+ * @ap: argument pointer
+ * @params: the parameters struct
+ * Return: number chars printed
  */
-int print_int(va_list l, flags_t *f)
-{
-    int n = va_arg(l, int);
-    int res = count_digit(n);
 
-    if (f->space == 1 && f->plus == 0 && n >= 0)
-        res += _putchar(' ');
-    if (f->plus == 1 && n >= 0)
-        res += _putchar('+');
-    if (n <= 0)
-        res++;
-    print_number(n);
-    return (res);
+int print_char(va_list ap, params_t *params)
+{
+	char pad_char = ' ';
+	unsigned int pad = 1, sum = 0, ch = va_arg(ap, int);
+
+	if (params->minus_flag)
+		sum += _putchar(ch);
+	while (pad++ < params->width)
+		sum += _putchar(pad_char);
+	if (!params->minus_flag)
+		sum += _putchar(ch);
+	return (sum);
 }
 
 /**
- * print_unsigned - prints an unsigned integer
- * @l: va_list of arguments from _printf
- * @f: pointer to the struct flags determining
- * if a flag is passed to _printf
- * Return: number of char printed
+ * print_int - prints integer
+ * @ap: argument pointer
+ * @params: the parameters struct
+ * Return: number chars printed
  */
-int print_unsigned(va_list l, flags_t *f)
-{
-    unsigned int u = va_arg(l, unsigned int);
-    char *str = convert(u, 10, 0);
 
-    (void)f;
-    return (_puts(str));
+int print_int(va_list ap, params_t *params)
+{
+	long l;
+
+	if (params->l_modifier)
+		l = va_arg(ap, long);
+	else if (params->h_modifier)
+		l = (short int)va_arg(ap, int);
+	else
+		l = (int)va_arg(ap, int);
+	return (print_number(convert(l, 10, 0, params), params));
 }
 
 /**
- * print_number - helper function that loops through
- * an integer and prints all its digits
- * @n: integer to be printed
+ * print_string - prints string
+ * @ap: argument pointer
+ * @params: the parameters struct
+ * Return: number chars printed
  */
-void print_number(int n)
+
+int print_string(va_list ap, params_t *params)
 {
-    unsigned int n1;
+	char *str = va_arg(ap, char *), pad_char = ' ';
+	unsigned int pad = 0, sum = 0, i = 0, j;
 
-    if (n < 0)
-    {
-        _putchar('-');
-        n1 = -n;
-    }
-    else
-        n1 = n;
+	(void)params;
+	switch ((int)(!str))
+	case 1:
+		str = NULL_STRING;
 
-    if (n1 / 10)
-        print_number(n1 / 10);
-    _putchar((n1 % 10) + '0');
+		j = pad = _strlen(str);
+		if (params->precision < pad)
+			j = pad = params->precision;
+
+		if (params->minus_flag)
+		{
+			if (params->precision != UINT_MAX)
+				for (i = 0; i < pad; i++)
+					sum += _putchar(*str++);
+			else
+				sum += _puts(str);
+		}
+		while (j++ < params->width)
+			sum += _putchar(pad_char);
+		if (!params->minus_flag)
+		{
+			if (params->precision != UINT_MAX)
+				for (i = 0; i < pad; i++)
+					sum += _putchar(*str++);
+			else
+				sum += _puts(str);
+		}
+		return (sum);
 }
 
 /**
- * count_digit - returns the number of digits in an integer
- * for _printf
- * @i: integer to evaluate
- * Return: number of digits
+ * print_percent - prints string
+ * @ap: argument pointer
+ * @params: the parameters struct
+ * Return: number chars printed
  */
-int count_digit(int i)
-{
-    unsigned int d = 0;
-    unsigned int u;
 
-    if (i < 0)
-        u = i * -1;
-    else
-        u = i;
-    while (u != 0)
-    {
-        u /= 10;
-        d++;
-    }
-    return (d);
+int print_percent(va_list ap, params_t *params)
+{
+	(void)ap;
+	(void)params;
+	return (_putchar('%'));
+}
+
+/**
+ * print_S - custom format specifier
+ * @ap: argument pointer
+ * @params: the parameters struct
+ * Return: number chars printed
+ */
+
+int print_S(va_list ap, params_t *params)
+{
+	char *str = va_arg(ap, char *);
+	char *hex;
+	int sum = 0;
+
+	if ((int)(!str))
+		return (_puts(NULL_STRING));
+	for (; *str; str++)
+	{
+		if ((*str > 0 && *str < 32) || *str >= 127)
+		{
+			sum += _putchar('\\');
+			sum += _putchar('x');
+			hex = convert(*str, 16, 0, params);
+			if (!hex[1])
+				sum += _putchar('0');
+			sum += _puts(hex);
+		}
+		else
+		{
+			sum += _putchar(*str);
+		}
+	}
+	return (sum);
 }

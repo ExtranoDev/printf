57efd8a951b5f5254cdf66779c79bc1dc4acb988
Oguntola Joshua <101347375+ExtranoDev@users.noreply.github.com>
2022-05-25 12:57:44 -0700

    Updated

--- _printID.c
+++ _printID.c
@@ -1,34 +1,28 @@
 #include "main.h"
 
 /**
- * printID - help prints character format
- * @format1: char after %
- * @args: arguement for the type of format
- * Return: count of char printed
+ * convert - converts number and base into string
+ * @num: input number
+ * @base: input base
+ * @lowercase: flag if hexa values need to be lowercase
+ * Return: result string
  */
-int printID(char format1, va_list args)
+char *convert(unsigned long int num, int base, int lowercase)
 {
-	int i;
+    static char *rep;
+    static char buffer[50];
+    char *ptr;
 
-	structID ids[] = {
-		{"s", print_str},
-		{"c", print_char},
-		{"d", print_int},
-		{"i", print_int},
-		{"b", print_bin},
-		{"o", print_oct},
-		{"u", print_unsigned},
-		{"x", print_hex},
-		{"X", print_HEX},
-		{"S", print_STR},
-		{NULL, NULL}
-	};
+    rep = (lowercase)
+? "0123456789abcdef"
+: "0123456789ABCDEF";
+    ptr = &buffer[49];
+    *ptr = '\0';
+    do
+    {
+        *--ptr = rep[num % base];
+        num /= base;
+    } while (num != 0);
 
-	
-	for (i = 0; ids[i].ID != NULL; i++)
-	{
-		if (ids[i].ID[0] == format1)
-			return (ids[i].outter(args));
-	}
-	return (0);
+    return (ptr);
 }

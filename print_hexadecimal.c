77038f9a97c0df47675461bfd0a3967b46005cb0
Oguntola Joshua <101347375+ExtranoDev@users.noreply.github.com>
2022-05-25 12:35:51 -0700

    Updated

--- print_hexadecimal.c
+++ print_hexadecimal.c
@@ -91,77 +91,41 @@ int print_hex_big(va_list l, flags_t *f)
 
 
 /**
-
- * print_binary - prints a number in base 2
-
- * @l: va_list arguments from _printf
-
- * @f: pointer to the struct that determines
-
- * if a flag is passed to _printf
-
- * Description: the function calls convert() which in turns converts the input
-
- * number into the correct base and returns it as a string
-
- * Return: the number of char printed
-
+ * print_oct - prints number in octal base.
+ * @arg: list of num in octal base
+ * Return: number of octals printed
  */
 
-int print_binary(va_list l, flags_t *f)
-
+int print_oct(va_list arg)
 {
+	unsigned int num = va_arg(arg, unsigned int);
+	unsigned int dup;
+	char *octa;
+	int i, j, charPrinted = 0;
 
-    unsigned int num = va_arg(l, unsigned int);
+	if (num == 0)
+		return (_putchar('0'));
+	for (dup = num; dup != 0; j++)
+	{
+		dup /= 8;
+	}
+	octa = malloc(j);
+	if (!octa)
+		return (-1);
 
-    char *str = convert(num, 2, 0);
+	for (i = j - 1; i >= 0; i--)
+	{
+		octa[i] = num % 8 + '0';
+		num = num / 8;
+	}
 
-
-
-    (void)f;
-
-    return (_puts(str));
-
-}
-
-
-
-/**
-
- * print_octal - prints a number in base 8
-
- * @l: va_list arguments from _printf
-
- * @f: pointer to the struct that determines
-
- * if a flag is passed to _printf
-
- * Description: the function calls convert() which in turns converts the input
-
- * number into the correct base and returns it as a string
-
- * Return: the number of char printed
-
- */
-
-int print_octal(va_list l, flags_t *f)
-
-{
-
-    unsigned int num = va_arg(l, unsigned int);
-
-    char *str = convert(num, 8, 0);
-
-    int count = 0;
-
-
-
-    if (f->hash == 1 && str[0] != '0')
-
-        count += _putchar('0');
-
-    count += _puts(str);
-
-    return (count);
-
+	for (i = 0; i < j && octa[i] == '0'; i++)
+		;
+	for (; i < j; i++)
+	{
+		_putchar(octa[i]);
+		charPrinted++;
+	}
+	free(octa);
+	return (charPrinted);
 }

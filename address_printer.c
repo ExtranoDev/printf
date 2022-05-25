1e0bf91c953d3755edb4f93d419ba3109a381f2c
Oguntola Joshua <101347375+ExtranoDev@users.noreply.github.com>
2022-05-25 14:16:21 -0700

    Update address printer

--- address_printer.c
+++ address_printer.c
@@ -1,25 +1,20 @@
 #include "main.h"
 
 /**
- * print_address - prints address of input in hexa format
- * @l: va_list arguments from _printf
- * @f: pointer to the struct flags that determines
- * if a flag is passed to _printf
- * Return: number of char printed
+ * print_address - prints address
+ * @ap: argument pointer
+ * @params: the parameters struct
+ *
+ * Return: bytes printed
  */
-int print_address(va_list l, flags_t *f)
+int print_address(va_list ap, params_t *params)
 {
-    char *str;
-    unsigned long int p = va_arg(l, unsigned long int);
-
-    register int count = 0;
-
-    (void)f;
-
-    if (!p)
-        return (_puts("(nil)"));
-    str = convert(p, 16, 1);
-    count += _puts("0x");
-    count += _puts(str);
-    return (count);
+	unsigned long int n = va_arg(ap, unsigned long int);
+	char *str;
+	if (!n)
+		return (_puts("(nil)"));
+	str = convert(n, 16, CONVERT_UNSIGNED | CONVERT_LOWERCASE, params);
+	*--str = 'x';
+	*--str = '0';
+	return (print_number(str, params));
 }

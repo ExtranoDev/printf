7071135809eff1782b9555f7e96e8011b42f3fd9
Oguntola Joshua <101347375+ExtranoDev@users.noreply.github.com>
2022-05-25 14:25:16 -0700

    more converter added

--- conv_to_bin.c
+++ conv_to_bin.c
@@ -38,3 +38,52 @@ int print_unsigned(va_list ap, params_t *params)
 	params->unsign = 1;
 	return (print_number(convert(l, 10, CONVERT_UNSIGNED, params), params));
 }
+/**
+ * get_print_func - finds the format function
+ * @s: string of the format
+ * @ap: argument pointer
+ * @params: the parameters struct
+ * Return: the number of bytes printed
+ */
+
+int get_print_func(char *s, va_list ap, params_t *params)
+{
+	int (*f)(va_list, params_t *) = get_specifier(s);
+
+	if (f)
+		return (f(ap, params));
+	return (0);
+}
+
+/**
+ * get_flag - finds the flag functions
+ * @s: the format string
+ * @params: the parameters struct
+ * Return: if flag was valid
+ */
+
+int get_flag(char *s, params_t *params)
+
+{
+	int i = 0;
+
+	switch (*s)
+	{
+		case '+':
+			i = params->plus_flag = 1;
+			break;
+		case ' ':
+			i = params->space_flag = 1;
+			break;
+		case '#':
+			i = params->hashtag_flag = 1;
+			break;
+		case '-':
+			i = params->minus_flag = 1;
+			break;
+		case '0':
+			i = params->zero_flag = 1;
+			break;
+	}
+	return (i);
+}
